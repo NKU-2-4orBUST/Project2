e54e6680c91a6d14d7632267805fb9354e455a4d
atrenkamp <85128870+atrenkamp@users.noreply.github.com>
2021-06-05 11:38:34 -0400

    Update install-server.py

--- install-server.py
+++ install-server.py
@@ -1,4 +1,4 @@
-from os import system as sys, from os import chdir, import argparse
+import os, argparse
 #Creates Command line Argument Parsers
 parser=argparse.ArgumentParser(usage='Quickly install or reinstall servers',description='Used to install 1 or more servers including: Apache 2.4, NFS, and LDAP  |  Format: python3 ./install-server [-l,--ldap][-n,--nfs][-a,--apache]',add_help=True,allow_abbrev=True)
 parser.add_argument('-l','--ldap', help='installs and Configure LDAP services using openldap and its dependencies.',action='store_const', const='ldap')
@@ -8,38 +8,38 @@ args = parser.parse_args()
 
 def ldap():
 	# Installs software and Downloads files
-	sys('yum install -y openldap-servers openldap-clients nss_ldap migrationtools; wget https://github.com/NKU-2-4orBUST/Project2/raw/main/olc2 http://faculty.cs.nku.edu/~waldenj/classes/2011/summer/cit470/labs/example.ldif https://github.com/NKU-2-4orBUST/Project2/raw/main/olc2 https://github.com/NKU-2-4orBUST/Project2/raw/main/base.ldif https://github.com/NKU-2-4orBUST/Project2/raw/main/diradm.conf https://github.com/NKU-2-4orBUST/Project2/raw/main/migrate_common.ph http://www.hits.at/diradm/diradm-1.3.tar.gz')
+	os.system('yum install -y openldap-servers openldap-clients nss_ldap migrationtools; wget https://github.com/NKU-2-4orBUST/Project2/raw/main/olc2 http://faculty.cs.nku.edu/~waldenj/classes/2011/summer/cit470/labs/example.ldif https://github.com/NKU-2-4orBUST/Project2/raw/main/olc2 https://github.com/NKU-2-4orBUST/Project2/raw/main/base.ldif https://github.com/NKU-2-4orBUST/Project2/raw/main/diradm.conf https://github.com/NKU-2-4orBUST/Project2/raw/main/migrate_common.ph http://www.hits.at/diradm/diradm-1.3.tar.gz')
 	# Sets Configurations for LDAP server
-	sys('cat /root/olc2 > /etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif; rm /var/lib/ldap/*; cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG; cat ./migrate_common.ph > /usr/share/migrationtools/migrate_common.ph; cat ./base.ldif > /usr/share/migrationtools/base.ldif; tar -xvf diradm-1.3.tar.gz -C /usr/local; cp /usr/local/diradm-1.3/diradm.conf /etc/; cat ./diradm.conf > /etc/diradm.conf')
+	os.system('cat /root/olc2 > /etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif; rm /var/lib/ldap/*; cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG; cat ./migrate_common.ph > /usr/share/migrationtools/migrate_common.ph; cat ./base.ldif > /usr/share/migrationtools/base.ldif; tar -xvf diradm-1.3.tar.gz -C /usr/local; cp /usr/local/diradm-1.3/diradm.conf /etc/; cat ./diradm.conf > /etc/diradm.conf')
 	# Migrates system user and groups to LDAP services and set the owner of all files in /var/lib/ldap as ldap
-	sys('systemctl start slapd; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/core.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/cosine.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/nis.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/inetorgperson.ldif; systemctl stop slapd; chown -R ldap:ldap /var/lib/ldap; slapadd -v -l /usr/share/migrationtools/base.ldif; /usr/share/migrationtools/migrate_passwd.pl /etc/passwd > /usr/share/migrationtools/passwd.ldif; slapadd -v -l /usr/share/migrationtools/passwd.ldif; /usr/share/migrationtools/migrate_group.pl /etc/group > /usr/share/migrationtools/group.ldif; slapadd -v -l /usr/share/migrationtools/group.ldif; chown -R ldap:ldap /var/lib/ldap; systemctl start slapd; systemctl -l status slapd')
+	os.system('systemctl start slapd; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/core.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/cosine.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/nis.ldif; ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/inetorgperson.ldif; systemctl stop slapd; chown -R ldap:ldap /var/lib/ldap; slapadd -v -l /usr/share/migrationtools/base.ldif; /usr/share/migrationtools/migrate_passwd.pl /etc/passwd > /usr/share/migrationtools/passwd.ldif; slapadd -v -l /usr/share/migrationtools/passwd.ldif; /usr/share/migrationtools/migrate_group.pl /etc/group > /usr/share/migrationtools/group.ldif; slapadd -v -l /usr/share/migrationtools/group.ldif; chown -R ldap:ldap /var/lib/ldap; systemctl start slapd; systemctl -l status slapd')
 	# Configures Firewall for LDAP service
-	sys('firewall-cmd --zone=public --add-port=389/tcp --permanent; firewall-cmd --zone=public --add-port=636/tcp --permanent; firewall-cmd --reload')
+	os.system('firewall-cmd --zone=public --add-port=389/tcp --permanent; firewall-cmd --zone=public --add-port=636/tcp --permanent; firewall-cmd --reload')
 def nfs():
 	# Downloading configuration files and installing NFS  erase if no longer needed -- https://github.com/NKU-2-4orBUST/Project2/raw/main/fstab.txt
-	sys('yum install -y nfs-utils rpcbin; wget https://github.com/NKU-2-4orBUST/Project2/raw/main/exports.txt; exportfs -a  ')
+	os.system('yum install -y nfs-utils rpcbin; wget https://github.com/NKU-2-4orBUST/Project2/raw/main/exports.txt; exportfs -a  ')
 	# Exporting /HOME directory and starting NFS and required services
-	sys('cat exports.txt > /etc/exports; systemctl start nfs; systemctl start nfslock') 
+	os.system('cat exports.txt > /etc/exports; systemctl start nfs; systemctl start nfslock') 
 	#Firewall configured and reloaded.
-	sys('firewall-cmd --zone=public --add-port=2049/tcp --permanent; firewall-cmd --zone=public --add-port=111/tcp --permanent; firewall-cmd --zone=public --add-port=20048/tcp --permanent; firewall-cmd --zone=public --add-port=2049/udp --permanent; firewall-cmd --zone=public --add-port=111/udp --permanent; firewall-cmd --zone=public --add-port=20048/udp --permanent; systemctl restart firewalld')
+	os.system('firewall-cmd --zone=public --add-port=2049/tcp --permanent; firewall-cmd --zone=public --add-port=111/tcp --permanent; firewall-cmd --zone=public --add-port=20048/tcp --permanent; firewall-cmd --zone=public --add-port=2049/udp --permanent; firewall-cmd --zone=public --add-port=111/udp --permanent; firewall-cmd --zone=public --add-port=20048/udp --permanent; systemctl restart firewalld')
 
 def apache():	
 	#Downloads required Configs and Installs software
-	sys('echo Installing Apache 2.4 and Mod_ssl.... | tee  -a /root/apache_install.log; yum -y install httpd mod_ssl >> /root/apache_install.log 2>&1; wget https://github.com/NKU-2-4orBUST/Project1/raw/main/httpd.conf https://github.com/NKU-2-4orBUST/Project1/raw/main/userdir.conf >> /root/apache_install.log 2>&1')
+	os.system('echo Installing Apache 2.4 and Mod_ssl.... | tee  -a /root/apache_install.log; yum -y install httpd mod_ssl >> /root/apache_install.log 2>&1; wget https://github.com/NKU-2-4orBUST/Project1/raw/main/httpd.conf https://github.com/NKU-2-4orBUST/Project1/raw/main/userdir.conf >> /root/apache_install.log 2>&1')
 	#Configures apache 2.4 web server
-	sys('echo Configuring Apache Webserver... | tee -a /root/apache_install.log; \\cp httpd.conf /etc/httpd/conf >> /root/apache_install.log; \\cp userdir.conf /etc/httpd/conf.d/ >> /root/apache_install.log; mkdir /etc/httpd/ssl') #Replaces apache config and userdir.conf with know good config
+	os.system('echo Configuring Apache Webserver... | tee -a /root/apache_install.log; \\cp httpd.conf /etc/httpd/conf >> /root/apache_install.log; \\cp userdir.conf /etc/httpd/conf.d/ >> /root/apache_install.log; mkdir /etc/httpd/ssl') #Replaces apache config and userdir.conf with know good config
 	#Changes to new SLL directory to generate SSL keys in the next step
-	chdir('/etc/httpd/ssl')#Changes to SSL directory
+	os.chdir('/etc/httpd/ssl')#Changes to SSL directory
 	#Generates SSL keys for HTTPS 
-	sys('openssl genrsa -out 10_2_7_71_Group_2.key 1024 >> /root/apache_install.log 2>&1; openssl req -new -key 10_2_7_71_Group_2.key -subj "/C=US/ST=Kentucky/L=Highland Heights/O=NKU/CN=10-2-7-71-Group-2" -out 10_2_7_71_Group_2.csr; openssl x509 -req -days 365 -in 10_2_7_71_Group_2.csr -signkey 10_2_7_71_Group_2.key -out 10_2_7_71_Group_2.crt >> /root/ApacheInstallScriptLog 2>&1')# Creates .key file for SSL 
+	os.system('openssl genrsa -out 10_2_7_71_Group_2.key 1024 >> /root/apache_install.log 2>&1; openssl req -new -key 10_2_7_71_Group_2.key -subj "/C=US/ST=Kentucky/L=Highland Heights/O=NKU/CN=10-2-7-71-Group-2" -out 10_2_7_71_Group_2.csr; openssl x509 -req -days 365 -in 10_2_7_71_Group_2.csr -signkey 10_2_7_71_Group_2.key -out 10_2_7_71_Group_2.crt >> /root/ApacheInstallScriptLog 2>&1')# Creates .key file for SSL 
 	#Configures the firewall and starts apache webserver
-	sys('echo Configuring Firewall for Apache | tee -a /root/apache_install.log; sudo firewall-cmd --permanent --add-port=80/tcp >> /root/apache_install.log 2>&1; sudo firewall-cmd --permanent --add-port=443/tcp >> /root/apache_install.log 2>&1; sudo firewall-cmd --reload>> /root/apache_install.log 2>&1; echo Waiting for Firewall to reload | tee  -a /root/apache_install.log; sleep 5; echo Firewall reloaded | tee  -a /root/apache_install.log; systemctl -l status firewalld >> /root/apache_install.log 2>&1; systemctl start httpd >> /root/apache_install.log 2>&1; systemctl status httpd >> /root/apache_install.log 2>&1 ') 
+	os.system('echo Configuring Firewall for Apache | tee -a /root/apache_install.log; sudo firewall-cmd --permanent --add-port=80/tcp >> /root/apache_install.log 2>&1; sudo firewall-cmd --permanent --add-port=443/tcp >> /root/apache_install.log 2>&1; sudo firewall-cmd --reload>> /root/apache_install.log 2>&1; echo Waiting for Firewall to reload | tee  -a /root/apache_install.log; sleep 5; echo Firewall reloaded | tee  -a /root/apache_install.log; systemctl -l status firewalld >> /root/apache_install.log 2>&1; systemctl start httpd >> /root/apache_install.log 2>&1; systemctl status httpd >> /root/apache_install.log 2>&1 ') 
 	#Configures system settings to allow public traffic.
-	sys('setsebool -P httpd_enable_homedirs on; chcon -R -t httpd_sys_content_t /home/; chcon -R -t httpd_sys_rw_content_t /home/')
+	os.system('setsebool -P httpd_enable_homedirs on; chcon -R -t httpd_sys_content_t /home/; chcon -R -t httpd_sys_rw_content_t /home/')
 
 def main(args):
 	#Changes pythons to tmp directory save files just for the install
-	chdir('/tmp')
+	os.chdir('/tmp')
     	#Starts LDAP Install
     	if args.ldap == 'ldap': 
         	print('Installing LDAP ')
